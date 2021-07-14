ddc9605a012dca3884070c0d0e05f5da377e9a88
Gökhan İMEN <70821916+gokhanimen@users.noreply.github.com>
2021-07-14 14:52:52 +0300

    up scroll button has been created and to all pages in added.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,8 +7,23 @@ hamburgerBtn.addEventListener("click", () => {
     document.body.style.position = "fixed";
 })
 hamburgerCloseBtn.addEventListener("click", () => {
-    if (navigationMenu.classList.contains("-opened")) {
-        navigationMenu.classList.remove("-opened");
-        document.body.style.position = "static";
+        if (navigationMenu.classList.contains("-opened")) {
+            navigationMenu.classList.remove("-opened");
+            document.body.style.position = "static";
+        }
+    })
+    // Scroll top button
+var mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
     }
-})
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
